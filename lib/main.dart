73621e2051118b9dc43087e423530725f6d28600
Carlos Carvalho <carlos.brasilia@gmail.com>
2021-02-14 22:10:03 -0300

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[600],
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: MagicBall(),
+      ),
+    ),
+  );
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int imageNumber = 1;
+
+  void changeImage() {
+    setState(() {
+      imageNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    changeImage();
+                  },
+                  child: Image.asset('images/ball$imageNumber.png'))),
+        ],
       ),
     );
+  }
+}
